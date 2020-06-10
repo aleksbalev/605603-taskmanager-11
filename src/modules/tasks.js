6d04eae89cf5e1fbb4c3b7db45598ea17db769a6
AleksBalev <aleks.balev7899@gmail.com>
2020-06-10 10:53:13 +0200

    teach model to consider chosen filter

--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -1,17 +1,35 @@
+import {
+  getTasksByFilter
+} from '../utils/filter';
+import {
+  FilterType
+} from '../utils/const';
+
 export default class Tasks {
   constructor() {
     this._tasks = [];
+    this._activeFilterType = FilterType.ALL;
 
     this._dataChangeHandler = [];
+    this._filterChangeHandlers = [];
   }
 
   getTasks() {
+    return getTasksByFilter(this._tasks, this._activeFilterType);
+  }
+
+  getTasksAll() {
     return this._tasks;
   }
 
   setTasks(tasks) {
     this._tasks = Array.from(tasks);
     this._callHandlers(this._dataChangeHandler);
+  }
+
+  setFilter(filterType) {
+    this._activeFilterType = filterType;
+    this._callHandlers(this._filterChangeHandlers);
   }
 
   updateTask(id, task) {
